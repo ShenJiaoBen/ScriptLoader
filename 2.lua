7ed9bf6639a1e914d3956bf884c716df8d48d8a5
ShenJiaoBen <1874787268@qq.com>
2025-05-03 08:34:14 +0800

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,5 +1,5 @@
 getgenv().Games = {
-  [155615604] = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/main/VIP.lua", --监狱人生
+  [3623096087] = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LN%E5%8A%9B%E9%87%8F.lua", --监狱人生
   [16732694052] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua", -- 鱼
   [7239319209] = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/new_ohio.lua", --俄亥俄州
 }
