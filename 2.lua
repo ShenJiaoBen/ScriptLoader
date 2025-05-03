535e9217ea6473f4300ea64896ec1c18dc8b82a4
ShenJiaoBen <1874787268@qq.com>
2025-05-03 10:48:42 +0800

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [] = "", --力量传奇
+  [3623096087] = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/LN%E5%8A%9B%E9%87%8F.lua", --力量传奇
   [16732694052] = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/FishScriptLn.lua", -- 鱼
   [7239319209] = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/new_ohio.lua"
 ,
