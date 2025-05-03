d896395bc160b4f9a6b605c3576a10512aa48c5f
ShenJiaoBen <1874787268@qq.com>
2025-05-03 10:46:16 +0800

    Update VIP.lua

--- VIP.lua
+++ VIP.lua
@@ -5,12 +5,18 @@ if type(Games) ~= "table" then
 end
 
 
-if game.PlaceId == 7239319209 then  -- 这里11111要替换成你实际需要的游戏ID
+if game.PlaceId == 7239319209 then  -- ohio
 KingScript = "By King" Roblox= "草拟吗"
 KingTeam= "King无敌" 
 end
 
--- 执行对应游戏的脚本
+elseif game.PlaceId == 16732694052 then  -- fish
+LnScript = "By LN" Roblox= "Fish"
+LnTeam= "免费"
+
+end
+
+
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
