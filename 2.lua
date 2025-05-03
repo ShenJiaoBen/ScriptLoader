bdbc81d845ad391fa1d038071db37e31117ac028
ShenJiaoBen <1874787268@qq.com>
2025-05-03 08:27:10 +0800

    Add files via upload

---
+++ 2.lua
@@ -0,0 +1,5 @@
+getgenv().Games = {
+  [155615604] = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/main/VIP.lua", --监狱人生
+  [16732694052] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua", -- 鱼
+  [7239319209] = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/new_ohio.lua", --俄亥俄州
+}
