5bf635c72c9bc949e8c60aae5ceca7fd59c16487
ShenJiaoBen <1874787268@qq.com>
2025-05-03 10:50:47 +0800

    Update VIP.lua

--- VIP.lua
+++ VIP.lua
@@ -1,10 +1,5 @@
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/ShenJiaoBen/ScriptLoader/refs/heads/main/2.lua"))()
 
-if type(Games) ~= "table" then
-    Games = {}  
-end
-
-
 if game.PlaceId == 7239319209 then  -- ohio
 KingScript = "By King" Roblox= "草拟吗"
 KingTeam= "King无敌" 
@@ -13,11 +8,11 @@ end
 elseif game.PlaceId == 16732694052 then  -- fish
 LnScript = "By LN" Roblox= "Fish"
 LnTeam= "免费"
+end
 
 elseif game.PlaceId == 3623096087 then  -- 力量传奇
 KingScript = "By LN" Roblox= "霖溺Script"
 KingTeam= "LegendofPower" 
-
 end
 
 
