3f87e6d590ec2bf3ed56e0b981738c55d3164fe7
ShenJiaoBen <1874787268@qq.com>
2025-05-03 10:44:41 +0800

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,6 +1,6 @@
 local Games = {
-  [155615604] = "", --监狱人生
-  [16732694052] = "", -- 鱼
+  [] = "", --力量传奇
+  [16732694052] = "https://raw.githubusercontent.com/ShenJiaoBen/Partial-Server-Ribbon/refs/heads/main/FishScriptLn.lua", -- 鱼
   [7239319209] = "https://raw.githubusercontent.com/ShenJiaoBen/ShenJiaoBen/refs/heads/main/new_ohio.lua"
 ,
 }
