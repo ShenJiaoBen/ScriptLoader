5966e86621c54d846c8b9f2d52e6828cb7a3b888
ShenJiaoBen <1874787268@qq.com>
2025-05-04 14:52:46 +0800

    Update 2.lua

--- 2.lua
+++ 2.lua
@@ -1,3 +1,10 @@
+local Games = {
+    ["Server"] = {
+        ids = {GameIDs},
+        url = "GameIDs"
+    }
+}
+
 local GamesById = {}
 for name, data in pairs(Games) do
     for _, id in ipairs(data.ids) do
